f97f3d8deabca93814c8c2cd0e90622da19af5aa
gomathisankar85 <gomathisankar.85@gmail.com>
2021-06-15 13:35:38 +0530

    Test case modified

--- src/test/java/com/gs/demo/MysqldemoApplicationTests.java
+++ src/test/java/com/gs/demo/MysqldemoApplicationTests.java
@@ -1,13 +1,25 @@
 package com.gs.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.gs.demo.entity.Employee;
+import com.gs.demo.repository.EmployeeRepository;
 
 @SpringBootTest
 class MysqldemoApplicationTests {
-
+	@Autowired
+	private EmployeeRepository employeeRepository;
+		
 	@Test
-	void contextLoads() {
+	void testEmployeeListing() {
+		Optional<Employee> emp = employeeRepository.findById((long) 2);
+		assertEquals("002", emp.get().getCode());
 	}
 
 }
